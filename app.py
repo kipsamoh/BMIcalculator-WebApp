08efaeb29b4e366e7c89d6d82250f4e63260e368
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-07-03 08:17:25 +0300

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, url_for
 from bmi_calculator import bmi_calculator_blueprint
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here'  # Replace with a secure key
+app.secret_key = 'kipsamoh8698'  # Replace with a secure key
 
 # Registering the blueprint for BMI calculator routes
 app.register_blueprint(bmi_calculator_blueprint, url_prefix='/bmi_calculator')
