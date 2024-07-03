dfe8e860205d1e18c24945ad9692c59614eb10e3
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-07-03 09:58:09 +0300

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,15 +1,11 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db, login_manager
-from models import User
+from app import db
+from models import User, Message
 from flask_login import login_user, logout_user, current_user, login_required
 
 # Blueprint definition
 bmi_calculator_blueprint = Blueprint('bmi_calculator', __name__)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 # Route for the Home page
 @bmi_calculator_blueprint.route('/')
