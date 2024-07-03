b28218d20709fd39564f00bfceed9e3b930ccbcc
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-07-03 09:57:27 +0300

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from bmi_calculator import bmi_calculator_blueprint
+from bmi_calc import bmi_calculator_blueprint
 
 # Initialize Flask application
 app = Flask(__name__)
@@ -20,16 +20,6 @@ app.register_blueprint(bmi_calculator_blueprint, url_prefix='/bmi_calculator')
 @app.route('/')
 def home():
     return redirect(url_for('bmi_calculator.home'))
-
-# Define your SQLAlchemy models
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
-    message = db.Column(db.Text, nullable=False)
-
-    def __repr__(self):
-        return f"Message('{self.name}', '{self.email}')"
 
 # Ensure the database tables are created before the first request
 @app.before_first_request
