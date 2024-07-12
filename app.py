f7cabbdaa5e84471817ccdc7c8e1f76d8ea014b9
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2024-07-12 16:17:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-    age = db.Column(db.Integer, nullable=False)  # Added age field
-    is_admin = db.Column(db.Boolean, default=False)  # Admin field added
+    age = db.Column(db.Integer, nullable=False)  # age field
+    is_admin = db.Column(db.Boolean, default=False)  # Admin field
     bmi_history = db.relationship('BMIHistory', backref='user', lazy=True)
     contact_messages = db.relationship('ContactMessage', backref='user', lazy=True)
 
